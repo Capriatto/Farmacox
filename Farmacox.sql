6fd2f2523102b40e072e29ba1e0ba504b9f68160
Capriatto <michu-1@hotmail.com>
2013-12-17 06:11:49 -0500

    cambios en base de datos

--- Farmacox.sql
+++ Farmacox.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-12-2013 a las 07:44:25
--- Versión del servidor: 5.5.32
--- Versión de PHP: 5.4.19
+-- Servidor: localhost
+-- Tiempo de generación: 17-12-2013 a las 10:53:35
+-- Versión del servidor: 5.6.12-log
+-- Versión de PHP: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `farmacox`
 --
-CREATE DATABASE IF NOT EXISTS `farmacox` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `farmacox` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `farmacox`;
 
 -- --------------------------------------------------------
@@ -192,27 +192,28 @@ INSERT INTO `proveedor` (`id`, `nombre_empresa`, `telefono`) VALUES
 CREATE TABLE IF NOT EXISTS `sintoma` (
   `id` int(11) NOT NULL,
   `nombre` varchar(30) DEFAULT NULL,
-  `producto_recomendado` char(3) NOT NULL,
-  PRIMARY KEY (`id`)
+  `producto_id` char(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_id` (`producto_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `sintoma`
 --
 
-INSERT INTO `sintoma` (`id`, `nombre`, `producto_recomendado`) VALUES
-(1, 'dolor de cabeza', '0'),
-(2, 'escalofríos', '0'),
-(3, 'tos ', '0'),
-(4, 'fiebre', '0'),
-(5, 'cansancio', '0'),
-(6, 'vómito', '0'),
-(7, 'náuseas', '0'),
-(8, 'perdida de apetito', '0'),
-(9, 'dolores musculares', '0'),
-(10, 'perdida de peso', '0'),
-(11, 'orinar frecuentemente', '0'),
-(12, 'mucha hambre', '0');
+INSERT INTO `sintoma` (`id`, `nombre`, `producto_id`) VALUES
+(1, 'dolor de cabeza', '001'),
+(2, 'escalofríos', '001'),
+(3, 'tos ', '001'),
+(4, 'fiebre', '002'),
+(5, 'cansancio', '001'),
+(6, 'vómito', '001'),
+(7, 'náuseas', '001'),
+(8, 'perdida de apetito', '001'),
+(9, 'dolores musculares', '001'),
+(10, 'perdida de peso', '001'),
+(11, 'orinar frecuentemente', '001'),
+(12, 'mucha hambre', '001');
 
 -- --------------------------------------------------------
 
@@ -254,21 +255,89 @@ CREATE TABLE IF NOT EXISTS `venta` (
   `usuario_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`usuario_id`),
   KEY `fk_venta_usuario1_idx` (`usuario_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=51 ;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`id`, `hora_venta`, `fecha_venta`, `productos`, `valor_venta`, `usuario_id`) VALUES
+(1, '00:00:00', '2013-12-17', 1, 400, 1),
+(2, '12:41:05', '2013-12-17', 2, 400, 1),
+(3, '12:41:05', '2013-12-17', 3, 400, 2),
+(4, '12:45:05', '2013-12-17', 3, 400, 2),
+(5, '01:03:05', '2013-12-17', 1, 400, 1),
+(6, '01:44:05', '2013-12-17', 1, 400, 1),
+(7, '02:42:53', '2013-12-17', 1, 150, 4),
+(8, '02:43:25', '2013-12-17', 2, 300, 4),
+(9, '03:29:32', '2013-12-17', 1, 150, 4),
+(10, '03:29:49', '2013-12-17', 2, 300, 4),
+(11, '03:32:51', '2013-12-17', 1, 150, 4),
+(12, '03:32:51', '2013-12-17', 1, 150, 4),
+(13, '03:34:34', '2013-12-17', 2, 300, 4),
+(14, '03:34:34', '2013-12-17', 2, 300, 4),
+(15, '03:35:38', '2013-12-17', 3, 700, 4),
+(16, '03:35:38', '2013-12-17', 3, 700, 4),
+(17, '03:37:19', '2013-12-17', 1, 150, 4),
+(18, '03:37:19', '2013-12-17', 1, 150, 4),
+(19, '03:57:43', '2013-12-17', 3, 700, 4),
+(20, '03:59:43', '2013-12-17', 3, 700, 4),
+(21, '04:00:22', '2013-12-17', 3, 700, 4),
+(22, '04:00:23', '2013-12-17', 3, 700, 4),
+(23, '04:00:24', '2013-12-17', 3, 700, 4),
+(24, '04:00:24', '2013-12-17', 3, 700, 4),
+(25, '04:00:24', '2013-12-17', 3, 700, 4),
+(26, '04:00:24', '2013-12-17', 3, 700, 4),
+(27, '04:00:25', '2013-12-17', 3, 700, 4),
+(28, '04:00:25', '2013-12-17', 3, 700, 4),
+(29, '04:00:25', '2013-12-17', 3, 700, 4),
+(30, '04:00:25', '2013-12-17', 3, 700, 4),
+(31, '04:00:26', '2013-12-17', 3, 700, 4),
+(32, '04:00:26', '2013-12-17', 3, 700, 4),
+(33, '04:00:26', '2013-12-17', 3, 700, 4),
+(34, '04:00:26', '2013-12-17', 3, 700, 4),
+(35, '04:00:27', '2013-12-17', 3, 700, 4),
+(36, '04:00:27', '2013-12-17', 3, 700, 4),
+(37, '04:00:27', '2013-12-17', 3, 700, 4),
+(38, '04:00:27', '2013-12-17', 3, 700, 4),
+(39, '04:08:49', '2013-12-17', 10, 450, 4),
+(40, '04:12:27', '2013-12-17', 3, 700, 4),
+(41, '04:41:10', '2013-12-17', 2, 300, 4),
+(42, '04:41:58', '2013-12-17', 5, 400, 4),
+(43, '04:41:59', '2013-12-17', 5, 400, 4),
+(44, '04:43:32', '2013-12-17', 12, 400, 4),
+(45, '04:44:21', '2013-12-17', 12, 400, 4),
+(46, '04:45:16', '2013-12-17', 2, 300, 4),
+(47, '04:48:27', '2013-12-17', 1, 150, 4),
+(48, '05:05:47', '2013-12-17', 1, 150, 4),
+(49, '05:06:43', '2013-12-17', 5, 400, 2),
+(50, '05:07:08', '2013-12-17', 5, 400, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `venta-producto`
+-- Estructura de tabla para la tabla `venta_producto`
 --
 
-CREATE TABLE IF NOT EXISTS `venta-producto` (
+CREATE TABLE IF NOT EXISTS `venta_producto` (
   `venta_id` int(11) DEFAULT NULL,
   `venta_usuario_id` int(11) DEFAULT NULL,
   `producto_id` char(3) DEFAULT NULL,
   KEY `fk_venta_has_producto_producto1_idx` (`producto_id`),
   KEY `fk_venta_has_producto_venta1_idx` (`venta_id`,`venta_usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `venta_producto`
+--
+
+INSERT INTO `venta_producto` (`venta_id`, `venta_usuario_id`, `producto_id`) VALUES
+(7, 4, '002'),
+(7, 4, '003'),
+(7, 4, '001'),
+(3, 2, '005'),
+(3, 2, '005'),
+(3, 2, '005');
 
 --
 -- Restricciones para tablas volcadas
@@ -295,15 +364,21 @@ ALTER TABLE `producto`
   ADD CONSTRAINT `fk_producto_proveedor1` FOREIGN KEY (`proveedor_id`) REFERENCES `proveedor` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Filtros para la tabla `sintoma`
+--
+ALTER TABLE `sintoma`
+  ADD CONSTRAINT `sintoma_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`);
+
+--
 -- Filtros para la tabla `venta`
 --
 ALTER TABLE `venta`
   ADD CONSTRAINT `fk_venta_usuario1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Filtros para la tabla `venta-producto`
+-- Filtros para la tabla `venta_producto`
 --
-ALTER TABLE `venta-producto`
+ALTER TABLE `venta_producto`
   ADD CONSTRAINT `fk_venta_has_producto_producto1` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_venta_has_producto_venta1` FOREIGN KEY (`venta_id`, `venta_usuario_id`) REFERENCES `venta` (`id`, `usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
